ee2c7e76ca14d22d5aa5990106df3beb1a11e513
Arash Ashrafzadeh <arashmath16@gmail.com>
2019-10-08 18:55:40 +0330

    Update CDHM_numpy.py

--- CDHM_numpy.py
+++ CDHM_numpy.py
@@ -1,8 +1,11 @@
-# This program prints the the lower triangular matrix in Cholesky decomposition of -
+# This program prints the the lower-triangular matrix in Cholesky decomposition of -
 # a Hilbert matrix of any given order.
 
-import numpy as np # Here, to perform decomposition on matrix
-from scipy.linalg import hilbert # Here, to create Hilbert matrix with
+import numpy as np # To perform matrix decomposition
+from scipy.linalg import hilbert # To create Hilbert matrix
 
 n = int(input(" Please enter the order of Hilbert matrix: ")) # Inputting the order of Hilbert matrix
-print(np.linalg.cholesky(hilbert(n))) # Prints out the lower triangular matrix in Cholesky decomposition  for Hilbert matrix
+print(np.linalg.cholesky(hilbert(n))) # Prints out the lower-triangular matrix in Cholesky decomposition for Hilbert matrix
+
+# Lines 7 & 8 can be merged into one line as the following (but it violates the clean, readable coding paradigms I guess!):
+# print(np.linalg.cholesky(hilbert(int(input()))))
